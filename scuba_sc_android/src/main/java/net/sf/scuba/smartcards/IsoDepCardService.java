9315b17badaf6da93aa4b59bdc0a7acce6d18268
martijno <martijno@717097ad-464a-42c7-ab04-3dfc85175d78>
2016-04-05 07:57:22 +0000

    - Tabs & spaces.

--- scuba_sc_android/src/main/java/net/sf/scuba/smartcards/IsoDepCardService.java
+++ scuba_sc_android/src/main/java/net/sf/scuba/smartcards/IsoDepCardService.java
@@ -45,102 +45,102 @@ import android.os.Build;
  */
 public class IsoDepCardService extends CardService {
 
-	private static final long serialVersionUID = -8123218195642784731L;
+  private static final long serialVersionUID = -8123218195642784731L;
 
-	private IsoDep nfc;
-	private int apduCount;
+  private IsoDep nfc;
+  private int apduCount;
 
-	/**
-	 * Constructs a new card service.
-	 * 
-	 * @param terminal the card terminal to connect to
-	 */
-	public IsoDepCardService(IsoDep nfc) {
-		this.nfc = nfc;
-		apduCount = 0;
-	}
+  /**
+   * Constructs a new card service.
+   * 
+   * @param terminal the card terminal to connect to
+   */
+  public IsoDepCardService(IsoDep nfc) {
+    this.nfc = nfc;
+    apduCount = 0;
+  }
 
-	/**
-	 * Opens a session with the card.
-	 */
-	public void open() throws CardServiceException {
-		if (isOpen()) { return; }
-		try {
-			nfc.connect();
-			if (!nfc.isConnected()) { 
-				throw new CardServiceException("failed to connect");
-			}
-			state = SESSION_STARTED_STATE;
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new CardServiceException(e.toString());
-		}
-	}
+  /**
+   * Opens a session with the card.
+   */
+  public void open() throws CardServiceException {
+    if (isOpen()) { return; }
+    try {
+      nfc.connect();
+      if (!nfc.isConnected()) { 
+        throw new CardServiceException("failed to connect");
+      }
+      state = SESSION_STARTED_STATE;
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new CardServiceException(e.toString());
+    }
+  }
 
-	/**
-	 * Whether there is an open session with the card.
-	 */
-	public boolean isOpen() {
-		if (nfc.isConnected()) {
-			state = SESSION_STARTED_STATE;
-			return true;
-		} else {
-			state = SESSION_STOPPED_STATE;
-			return false;
-		}
-	}
+  /**
+   * Whether there is an open session with the card.
+   */
+  public boolean isOpen() {
+    if (nfc.isConnected()) {
+      state = SESSION_STARTED_STATE;
+      return true;
+    } else {
+      state = SESSION_STOPPED_STATE;
+      return false;
+    }
+  }
 
-	/**
-	 * Sends an APDU to the card.
-	 * 
-	 * @param ourCommandAPDU the command apdu to send
-	 * @return the response from the card, including the status word
-	 * @throws CardServiceException - if the card operation failed 
-	 */
-	public ResponseAPDU transmit(CommandAPDU ourCommandAPDU) throws CardServiceException {
-		try {
-			if (!nfc.isConnected()) {
-				throw new CardServiceException("Not connected");
-			}
-			byte[] responseBytes = nfc.transceive(ourCommandAPDU.getBytes());
-			if (responseBytes == null || responseBytes.length < 2) {
-				throw new CardServiceException("Failed response");
-			}
-			ResponseAPDU ourResponseAPDU = new ResponseAPDU(responseBytes);
-			notifyExchangedAPDU(++apduCount, ourCommandAPDU, ourResponseAPDU);
-			return ourResponseAPDU;
-		} catch (IOException e) {
-			throw new CardServiceException(e.getMessage());
-		} catch (Exception e) {
-			throw new CardServiceException(e.getMessage());
-		}
-	}
+  /**
+   * Sends an APDU to the card.
+   * 
+   * @param ourCommandAPDU the command apdu to send
+   * @return the response from the card, including the status word
+   * @throws CardServiceException - if the card operation failed 
+   */
+  public ResponseAPDU transmit(CommandAPDU ourCommandAPDU) throws CardServiceException {
+    try {
+      if (!nfc.isConnected()) {
+        throw new CardServiceException("Not connected");
+      }
+      byte[] responseBytes = nfc.transceive(ourCommandAPDU.getBytes());
+      if (responseBytes == null || responseBytes.length < 2) {
+        throw new CardServiceException("Failed response");
+      }
+      ResponseAPDU ourResponseAPDU = new ResponseAPDU(responseBytes);
+      notifyExchangedAPDU(++apduCount, ourCommandAPDU, ourResponseAPDU);
+      return ourResponseAPDU;
+    } catch (IOException e) {
+      throw new CardServiceException(e.getMessage());
+    } catch (Exception e) {
+      throw new CardServiceException(e.getMessage());
+    }
+  }
 
-	public byte[] getATR() {
-		return null; // FIXME
-	}
+  public byte[] getATR() {
+    return null; // FIXME
+  }
 
-	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-	public boolean isExtendedAPDULengthSupported() {
-		if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN ) {
-			return nfc.isExtendedLengthApduSupported();
-		}
-		int maxTranceiveLength = nfc.getMaxTransceiveLength();
-		if (maxTranceiveLength > 261) {
-			return true;
-		}
-		return false;
-	}
+  @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+  public boolean isExtendedAPDULengthSupported() {
+    if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN ) {
+      return nfc.isExtendedLengthApduSupported();
+    }
+    int maxTranceiveLength = nfc.getMaxTransceiveLength();
+    if (maxTranceiveLength > 261) {
+      return true;
+    }
+    return false;
+  }
 
-	/**
-	 * Closes the session with the card.
-	 */
-	public void close() {
-		try {
-			nfc.close();
-			state = SESSION_STOPPED_STATE;
-		} catch (IOException e) {
-			/* Disconnect failed? Fine... */
-		}
-	}
+  /**
+   * Closes the session with the card.
+   */
+  public void close() {
+    try {
+      nfc.close();
+      state = SESSION_STOPPED_STATE;
+    } catch (IOException e) {
+      /* Disconnect failed? Fine... */
+    }
+  }
 }
